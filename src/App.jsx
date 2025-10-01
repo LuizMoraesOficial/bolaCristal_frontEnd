e7e7e96668915ef9ab10f9b72a6b9d4d60ca524e
Luiz Moraes <luizmoraesofc@gmail.com>
2025-10-01 20:58:06 -0300

    backup antes de preparar home

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>🚀 Plataforma React + Node</h1>
+      <p>FrontEnd conectado ao BackEnd</p>
+    </div>
+  );
+}
+
+export default App;
